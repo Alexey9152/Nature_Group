24ea2c724b9eea73b8f63f1cf21d122cc4ce7c5a
Alexey <loslesha0@gmail.com>
2025-06-30 16:59:59 +0300

    Описание изменений

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,16 @@
 #include "mainwindow.h"
-#include <QMainWindow>
-#include <QTextEdit>
-#include <QPushButton>
-#include <QLineEdit>
-#include <QVBoxLayout>
-#include <QHBoxLayout>
 #include <QFile>
+#include <QHBoxLayout>
+#include <QLineEdit>
+#include <QMainWindow>
+#include <QPushButton>
+#include <QTextEdit>
 #include <QTextStream>
+#include <QVBoxLayout>
 #include <algorithm>
 
-QTextStream& operator>>(QTextStream& in, Comp& item) {
+QTextStream &operator>>(QTextStream &in, Comp &item)
+{
     item.Name = in.readLine();
     item.Section = in.readLine();
     in >> item.Price;
@@ -17,12 +18,14 @@ QTextStream& operator>>(QTextStream& in, Comp& item) {
     return in;
 }
 
-QTextStream& operator<<(QTextStream& out, const Comp& item) {
+QTextStream &operator<<(QTextStream &out, const Comp &item)
+{
     out << item.Name << '\n' << item.Section << '\n' << item.Price << '\n';
     return out;
 }
 
-QTextStream& operator>>(QTextStream& in, Bag& item) {
+QTextStream &operator>>(QTextStream &in, Bag &item)
+{
     item.Name = in.readLine();
     in >> item.Num;
     in.readLine();
@@ -30,18 +33,23 @@ QTextStream& operator>>(QTextStream& in, Bag& item) {
     return in;
 }
 
-QTextStream& operator<<(QTextStream& out, const Bag& item) {
+QTextStream &operator<<(QTextStream &out, const Bag &item)
+{
     out << item.Name << '\n' << item.Num << '\n' << item.Section << '\n';
     return out;
 }
 
-template <class T>
-COMP<T>::COMP(const COMP& other) : count(other.count), arr(new T[other.count]) {
+template<class T>
+COMP<T>::COMP(const COMP &other)
+    : count(other.count)
+    , arr(new T[other.count])
+{
     std::copy(other.arr, other.arr + count, arr);
 }
 
-template <class T>
-COMP<T>& COMP<T>::operator=(const COMP& other) {
+template<class T>
+COMP<T> &COMP<T>::operator=(const COMP &other)
+{
     if (this != &other) {
         delete[] arr;
         count = other.count;
@@ -51,18 +59,21 @@ COMP<T>& COMP<T>::operator=(const COMP& other) {
     return *this;
 }
 
-template <class T>
-COMP<T>::~COMP() {
+template<class T>
+COMP<T>::~COMP()
+{
     delete[] arr;
 }
 
-template <class T>
-QString COMP<T>::printTable() const {
+template<class T>
+QString COMP<T>::printTable() const
+{
     return QString();
 }
 
 template<>
-QString COMP<Comp>::printTable() const {
+QString COMP<Comp>::printTable() const
+{
     QString result;
     QTextStream out(&result);
 
@@ -72,18 +83,14 @@ QString COMP<Comp>::printTable() const {
 
     out << TableTitle << "\n\n";
 
-    out << QString("Наименование").leftJustified(wName)
-        << QString("Секция").leftJustified(wSection)
-        << QString("Цена").rightJustified(wPrice)
-        << "\n";
+    out << QString("Наименование").leftJustified(wName) << QString("Секция").leftJustified(wSection)
+        << QString("Цена").rightJustified(wPrice) << "\n";
 
     out << QString(wName + wSection + wPrice, '-') << "\n";
 
     for (int i = 0; i < count; ++i) {
-        out << arr[i].Name.leftJustified(wName)
-        << arr[i].Section.leftJustified(wSection)
-        << QString::number(arr[i].Price).rightJustified(wPrice)
-        << "\n";
+        out << arr[i].Name.leftJustified(wName) << arr[i].Section.leftJustified(wSection)
+            << QString::number(arr[i].Price).rightJustified(wPrice) << "\n";
     }
 
     out << "\n";
@@ -91,7 +98,8 @@ QString COMP<Comp>::printTable() const {
 }
 
 template<>
-QString COMP<Bag>::printTable() const {
+QString COMP<Bag>::printTable() const
+{
     QString result;
     QTextStream out(&result);
 
@@ -102,25 +110,23 @@ QString COMP<Bag>::printTable() const {
     out << TableTitle << "\n\n";
 
     out << QString("Наименование").leftJustified(wName)
-        << QString("Количество").rightJustified(wNum)
-        << QString("Секция").rightJustified(wSection)
+        << QString("Количество").rightJustified(wNum) << QString("Секция").rightJustified(wSection)
         << "\n";
 
     out << QString(wName + wSection + wNum, '-') << "\n";
 
     for (int i = 0; i < count; ++i) {
-        out << arr[i].Name.leftJustified(wName)
-        << QString::number(arr[i].Num).rightJustified(wNum)
-        << arr[i].Section.rightJustified(wSection)
-        << "\n";
+        out << arr[i].Name.leftJustified(wName) << QString::number(arr[i].Num).rightJustified(wNum)
+            << arr[i].Section.rightJustified(wSection) << "\n";
     }
 
     out << "\n";
     return result;
 }
 
-template <class T>
-COMP<T> COMP<T>::operator+(const COMP& other) const {
+template<class T>
+COMP<T> COMP<T>::operator+(const COMP &other) const
+{
     std::set<QString> uniqueNames;
     std::vector<T> result;
 
@@ -146,8 +152,9 @@ COMP<T> COMP<T>::operator+(const COMP& other) const {
     return newComp;
 }
 
-template <class T>
-COMP<T> COMP<T>::operator-(const COMP& other) const {
+template<class T>
+COMP<T> COMP<T>::operator-(const COMP &other) const
+{
     std::set<QString> otherNames;
     std::vector<T> result;
 
@@ -169,21 +176,19 @@ COMP<T> COMP<T>::operator-(const COMP& other) const {
     return newComp;
 }
 
-template <class T>
-void COMP<T>::sortByName() {
-    std::sort(arr, arr + count, [](const T& a, const T& b) {
-        return a.Name < b.Name;
-    });
+template<class T>
+void COMP<T>::sortByName()
+{
+    std::sort(arr, arr + count, [](const T &a, const T &b) { return a.Name < b.Name; });
 }
 
-template <class T>
-void COMP<T>::sortSpecial() {
+template<class T>
+void COMP<T>::sortSpecial()
+{
     if (typeid(T) == typeid(Comp)) {
-        std::sort(arr, arr + count, [](const T& a, const T& b) {
-            return a.Name > b.Name;
-        });
+        std::sort(arr, arr + count, [](const T &a, const T &b) { return a.Name > b.Name; });
     } else {
-        std::sort(arr, arr + count, [](const T& a, const T& b) {
+        std::sort(arr, arr + count, [](const T &a, const T &b) {
             if (a.Section != b.Section)
                 return a.Section < b.Section;
             return a.Name > b.Name;
@@ -191,8 +196,9 @@ void COMP<T>::sortSpecial() {
     }
 }
 
-template <class T>
-T* COMP<T>::findByName(const QString& name) {
+template<class T>
+T *COMP<T>::findByName(const QString &name)
+{
     for (int i = 0; i < count; ++i) {
         if (arr[i].Name == name) {
             return &arr[i];
@@ -206,13 +212,16 @@ template class COMP<Comp>;
 template class COMP<Bag>;
 
 // Реализация методов MainWindow
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
     setupUI();
     loadData();
     showData();
 }
 
-void MainWindow::setupUI() {
+void MainWindow::setupUI()
+{
     setWindowTitle("Управление товарами");
     setMinimumSize(800, 600);
 
@@ -247,7 +256,8 @@ void MainWindow::setupUI() {
     connect(btnSearch, &QPushButton::clicked, this, &MainWindow::searchItem);
 }
 
-void MainWindow::loadData() {
+void MainWindow::loadData()
+{
     QFile file1("Ob1.txt");
     if (file1.open(QIODevice::ReadOnly | QIODevice::Text)) {
         QTextStream stream(&file1);
@@ -287,7 +297,8 @@ void MainWindow::loadData() {
     K3 = K1 + K2;
 }
 
-void MainWindow::showData() {
+void MainWindow::showData()
+{
     QString output;
     QTextStream stream(&output);
 
@@ -308,35 +319,42 @@ void MainWindow::showData() {
     textEdit->setText(output);
 }
 
-void MainWindow::mergeArrays() {
+void MainWindow::mergeArrays()
+{
     M3 = M1 + M2;
     K3 = K1 + K2;
     showData();
     statusBar()->showMessage("Массивы объединены");
 }
 
-void MainWindow::sortReverseAlphabetical() {
+void MainWindow::sortReverseAlphabetical()
+{
     M3.sortSpecial();
     showData();
     statusBar()->showMessage("Сортировка COMP в обратном алфавитном порядке");
 }
 
-void MainWindow::sortBySection() {
+void MainWindow::sortBySection()
+{
     K3.sortSpecial();
     showData();
     statusBar()->showMessage("Сортировка BAG по секциям и обратному алфавиту");
 }
 
-void MainWindow::searchItem() {
+void MainWindow::searchItem()
+{
     bool ok;
-    QString name = QInputDialog::getText(this, "Поиск товара",
+    QString name = QInputDialog::getText(this,
+                                         "Поиск товара",
                                          "Введите наименование товара:",
                                          QLineEdit::Normal,
-                                         "", &ok);
-    if (!ok || name.isEmpty()) return;
+                                         "",
+                                         &ok);
+    if (!ok || name.isEmpty())
+        return;
 
-    Comp* compItem = M3.findByName(name);
-    Bag* bagItem = K3.findByName(name);
+    Comp *compItem = M3.findByName(name);
+    Bag *bagItem = K3.findByName(name);
 
     QString result;
     if (compItem) {
--- mainwindow.h
+++ mainwindow.h
@@ -1,58 +1,61 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QFile>
+#include <QInputDialog>
 #include <QMainWindow>
-#include <QTextEdit>
+#include <QMessageBox>
 #include <QPushButton>
 #include <QStatusBar>
-#include <QInputDialog>
-#include <QMessageBox>
-#include <QFile>
+#include <QTextEdit>
 #include <QTextStream>
 #include <algorithm>
 #include <set>
-#include <vector>
 #include <typeinfo>
+#include <vector>
 
-struct Comp {
+struct Comp
+{
     QString Name;
     QString Section;
     int Price;
 
-    bool operator==(const Comp& other) const {
-        return Name == other.Name;
-    }
+    bool operator==(const Comp &other) const { return Name == other.Name; }
 };
 
-struct Bag {
+struct Bag
+{
     QString Name;
     int Num;
     QString Section;
 
-    bool operator==(const Bag& other) const {
-        return Name == other.Name;
-    }
+    bool operator==(const Bag &other) const { return Name == other.Name; }
 };
 
-QTextStream& operator>>(QTextStream& in, Comp& item);
-QTextStream& operator<<(QTextStream& out, const Comp& item);
-QTextStream& operator>>(QTextStream& in, Bag& item);
-QTextStream& operator<<(QTextStream& out, const Bag& item);
+QTextStream &operator>>(QTextStream &in, Comp &item);
+QTextStream &operator<<(QTextStream &out, const Comp &item);
+QTextStream &operator>>(QTextStream &in, Bag &item);
+QTextStream &operator<<(QTextStream &out, const Bag &item);
 
-template <class T>
-class COMP {
+template<class T>
+class COMP
+{
 private:
     inline static QString TableTitle;
     int count;
-    T* arr;
+    T *arr;
 
 public:
-    COMP() : count(0), arr(nullptr) {}
-    COMP(const COMP& other);
-    COMP& operator=(const COMP& other);
+    COMP()
+        : count(0)
+        , arr(nullptr)
+    {}
+    COMP(const COMP &other);
+    COMP &operator=(const COMP &other);
     ~COMP();
 
-    friend QTextStream& operator>>(QTextStream& in, COMP& comp) {
+    friend QTextStream &operator>>(QTextStream &in, COMP &comp)
+    {
         std::vector<T> temp;
         T item;
         while (!in.atEnd()) {
@@ -72,7 +75,8 @@ public:
         return in;
     }
 
-    friend QTextStream& operator<<(QTextStream& out, const COMP& comp) {
+    friend QTextStream &operator<<(QTextStream &out, const COMP &comp)
+    {
         for (int i = 0; i < comp.count; ++i) {
             out << comp.arr[i];
         }
@@ -80,23 +84,28 @@ public:
     }
 
     QString printTable() const;
-    COMP operator+(const COMP& other) const;
-    COMP operator-(const COMP& other) const;
+    COMP operator+(const COMP &other) const;
+    COMP operator-(const COMP &other) const;
     int GetCount() const { return count; }
     void sortByName();
     void sortSpecial();
-    T* findByName(const QString& name);
+    T *findByName(const QString &name);
 };
 
 // Объявляем явные специализации для printTable
-template<> QString COMP<Comp>::printTable() const;
-template<> QString COMP<Bag>::printTable() const;
+template<>
+QString COMP<Comp>::printTable() const;
+template<>
+QString COMP<Bag>::printTable() const;
 
 // Инициализация статических членов
-template<> QString COMP<Comp>::TableTitle = "ТОВАРЫ В МАГАЗИНЕ (COMP)";
-template<> QString COMP<Bag>::TableTitle = "ТОВАРЫ В МАГАЗИНЕ (BAG)";
+template<>
+QString COMP<Comp>::TableTitle = "ТОВАРЫ В МАГАЗИНЕ (COMP)";
+template<>
+QString COMP<Bag>::TableTitle = "ТОВАРЫ В МАГАЗИНЕ (BAG)";
 
-class MainWindow : public QMainWindow {
+class MainWindow : public QMainWindow
+{
     Q_OBJECT
 
 public:
